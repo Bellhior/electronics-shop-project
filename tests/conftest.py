576e17e8fcc135b97eb2537bad44374eded4e368
Bellhior <cliff_burton85@mail.ru>
2023-08-16 14:59:43 +0400

    some changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ from src.item import Item
 
 @pytest.fixture
 def item_1():
-    return Item("Смартфон", 10000, 20)
+    return Item("Смартфон", 10500, 20)
 
 
 @pytest.fixture
 def item_2():
-    return Item("Ноутбук", 20000, 5)
+    return Item("Ноутбук", 28000, 5)
