8808f371710d7524cbe4565f6ce4fe2d7e4a3fc3
Bellhior <cliff_burton85@mail.ru>
2023-08-16 12:28:43 +0400

    some changes

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item_1():
+    return Item("Смартфон", 10000, 20)
+
+
+@pytest.fixture
+def item_2():
+    return Item("Ноутбук", 20000, 5)
